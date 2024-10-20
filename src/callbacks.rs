5c7ed64cd727b6da365e2d8e9936aed1a9add693
Dillen Meijboom <dillen@brainhive.nl>
2024-10-20 20:07:03 +0200

    feat: improve progress bar on push

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -1,4 +1,11 @@
-use std::{env, str::FromStr};
+use std::{
+    env,
+    str::FromStr,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+};
 
 use git2::{Cred, RemoteCallbacks};
 use http::Uri;
@@ -33,28 +40,51 @@ fn get_credentials(url: &str, username: Option<&str>) -> Result<Cred, git2::Erro
     Cred::default()
 }
 
+#[derive(Default, Clone)]
+struct Progress {
+    total: Arc<AtomicUsize>,
+    current: Arc<AtomicUsize>,
+}
+
 pub fn remote_callbacks<'a>(
     stdout: &'a mut Vec<u8>,
     bar: &'a mut ProgressBar,
 ) -> RemoteCallbacks<'a> {
     let mut callbacks = RemoteCallbacks::new();
+    let pack = Progress::default();
+    let push = Progress::default();
+    let update = move |bar: &ProgressBar, pack: &Progress, push: &Progress| {
+        let current = pack.current.load(Ordering::Relaxed) + push.current.load(Ordering::Relaxed);
+        let total = pack.total.load(Ordering::Relaxed) + push.total.load(Ordering::Relaxed);
+
+        bar.set_length(total as u64);
+        bar.set_position(current as u64);
+    };
 
     callbacks.credentials(|url, username, _| get_credentials(url, username));
     callbacks.sideband_progress(|data| {
         stdout.extend_from_slice(data);
         true
     });
-    callbacks.pack_progress(|_stage, current, total| {
-        bar.set_message("Packing");
-        bar.set_length(total as u64);
-        bar.set_position(current as u64);
+
+    let pack_copy = pack.clone();
+    let bar_copy = bar.clone();
+    let push_copy = push.clone();
+
+    callbacks.pack_progress(move |_stage, current, total| {
+        bar_copy.set_message("Packing");
+        pack_copy.current.store(current, Ordering::Relaxed);
+        pack_copy.total.store(total, Ordering::Relaxed);
+
+        update(&bar_copy, &pack_copy, &push_copy);
     });
-    callbacks.push_transfer_progress(|current, total, _bytes| {
-        if !(total == 0 && current == 0) {
-            bar.set_message("Pushing");
-            bar.set_length(total as u64);
-            bar.set_position(current as u64);
-        }
+
+    callbacks.push_transfer_progress(move |current, total, _bytes| {
+        bar.set_message("Pushing");
+        push.current.store(current, Ordering::Relaxed);
+        push.total.store(total, Ordering::Relaxed);
+
+        update(bar, &pack, &push);
     });
 
     callbacks
